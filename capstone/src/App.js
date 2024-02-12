180dafb813facf437486d4ce2767a3340c3042af
sadel01 <nelsonsjf@gmail.com>
2024-02-12 15:11:00 -0300

    Capstone HTML and Meta Tags

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import React from "react";
+
+
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header/>
+        <Nav/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ capstone/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactComponent as Logo } from '../logo.svg';
+
+const Header = () => {
+  return (
+    <header>
+        <Logo />
+    </header>
+  );
+};
+export default Header;
