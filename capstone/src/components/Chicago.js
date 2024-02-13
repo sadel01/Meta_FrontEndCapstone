1dd74cd5fb7f23bc1dcb460a6661a69359be3bd3
sadel01 <nelsonsjf@gmail.com>
2024-02-13 20:51:55 -0300

    Last fixes

--- capstone/src/components/Chicago.js
+++ capstone/src/components/Chicago.js
@@ -9,7 +9,11 @@ const Chicago = () => {
                 <div className="aboutText">
                     <h1>Little Lemon</h1>
                     <h2>Chicago</h2>
-                    <p>At Little Lemon, our passion for gastronomy shines through in every dish we serve. From our humble beginnings, we've been committed to offering an exceptional culinary experience that celebrates fresh flavors and creativity in the kitchen. With a focus on high-quality, locally sourced ingredients and innovative culinary techniques, each visit to our restaurant is an opportunity to explore new flavors and delight in our unique creations. Whether you're seeking an intimate dinner, a family gathering, or a special occasion, we invite you to enjoy the warm hospitality and delicious cuisine of Little Lemon.</p>
+                    <p>At Little Lemon, our passion for gastronomy shines through in every dish we serve. 
+                        We've been committed to offering an exceptional culinary experience that celebrates fresh flavors and creativity in the kitchen. 
+                        With a focus on high-quality, locally sourced ingredients and innovative culinary techniques, 
+                        each visit to our restaurant is an opportunity to explore new flavors and delight in our unique creations. 
+                        We invite you to enjoy the warm hospitality and delicious cuisine of Little Lemon.</p>
                 </div>
                 <div className="imageContainer">
                     <img src={RestaurantChef} className="aboutImage" alt='Chef of Little Lemon Restaurant'></img>
